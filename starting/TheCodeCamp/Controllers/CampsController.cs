c882bc63ffd74c5cc747f9c79685f6c8266b2215
Neal Robben <neal.robben@cegeka.com>
2019-07-29 10:59:00 +0200

    Using status codes

---
+++ starting/TheCodeCamp/Controllers/CampsController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Http;
+
+namespace TheCodeCamp.Controllers
+{
+    public class CampsController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            return Ok(new { Name = "Shawn", Occupation = "Teacher" });
+        }
+    }
+}
