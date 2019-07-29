50ab664a3e8268428f4b85ceb5a0e1b107b9ea79
Neal Robben <neal.robben@cegeka.com>
2019-07-29 13:15:49 +0200

    Create an association controller

---
+++ starting/TheCodeCamp/Controllers/TalksController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Web.Http;
+using AutoMapper;
+using TheCodeCamp.Data;
+using TheCodeCamp.Models;
+
+namespace TheCodeCamp.Controllers
+{
+    [RoutePrefix("api/camps/{moniker}/talks")]
+    public class TalksController : ApiController
+    {
+        private readonly ICampRepository _repository;
+        private readonly IMapper _mapper;
+
+        public TalksController(ICampRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        [Route()]
+        public async Task<IHttpActionResult> Get(string moniker)
+        {
+            try
+            {
+                var results = await _repository.GetTalksByMonikerAsync(moniker);
+
+                return Ok(_mapper.Map<IEnumerable<TalkModel>>(results));
+            }
+            catch (Exception e)
+            {
+                return InternalServerError();
+            }
+        }
+    }
+}
