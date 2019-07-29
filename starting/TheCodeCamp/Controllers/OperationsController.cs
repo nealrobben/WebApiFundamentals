3c5a279bc2703d7c9a1c5abca9cbecf2d385f4cf
Neal Robben <neal.robben@cegeka.com>
2019-07-29 13:49:38 +0200

    Creating a functional API

---
+++ starting/TheCodeCamp/Controllers/OperationsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Configuration;
+using System.Web.Http;
+
+namespace TheCodeCamp.Controllers
+{
+    public class OperationsController : ApiController
+    {
+        [HttpOptions]
+        [Route("api/refreshconfig")]
+        public IHttpActionResult RefreshAppSettings()
+        {
+            try
+            {
+                ConfigurationManager.RefreshSection("AppSettings");
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return InternalServerError();
+            }
+        }
+    }
+}
