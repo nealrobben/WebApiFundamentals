e2f6e8a81a0ee1453eed28f8390837f3da2bd7f2
Neal Robben <neal.robben@cegeka.com>
2019-07-29 14:21:35 +0200

    URL versioning

--- starting/TheCodeCamp/App_Start/WebApiConfig.cs
+++ starting/TheCodeCamp/App_Start/WebApiConfig.cs
@@ -1,5 +1,8 @@
 ﻿using System.Web.Http;
+using System.Web.Http.Routing;
 using Microsoft.Web.Http;
+using Microsoft.Web.Http.Routing;
+using Microsoft.Web.Http.Versioning;
 using Newtonsoft.Json.Serialization;
 
 namespace TheCodeCamp
@@ -13,17 +16,27 @@ namespace TheCodeCamp
 
             config.AddApiVersioning(cfg =>
             {
-               cfg.DefaultApiVersion = new ApiVersion(1,1);
-               cfg.AssumeDefaultVersionWhenUnspecified = true;
-               cfg.ReportApiVersions = true;
+                cfg.DefaultApiVersion = new ApiVersion(1, 1);
+                cfg.AssumeDefaultVersionWhenUnspecified = true;
+                cfg.ReportApiVersions = true;
+                cfg.ApiVersionReader = ApiVersionReader.Combine(new HeaderApiVersionReader("X-Version"),
+                    new QueryStringApiVersionReader("ver"));
             });
 
             //Change case of JSON
             config.Formatters.JsonFormatter.SerializerSettings.ContractResolver =
                 new CamelCasePropertyNamesContractResolver();
 
+            var constraintResolver = new DefaultInlineConstraintResolver()
+            {
+                ConstraintMap =
+                {
+                    ["apiVersion"] = typeof(ApiVersionRouteConstraint)
+                }
+            };
+
             // Web API routes
-            config.MapHttpAttributeRoutes();
+            config.MapHttpAttributeRoutes(constraintResolver);
 
             //config.Routes.MapHttpRoute(
             //    name: "DefaultApi",
